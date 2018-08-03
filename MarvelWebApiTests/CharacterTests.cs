f352d2f699bb9e2b378ce98ecb02ae0b6b549c3a
rickyl7 <ricky7@gmail.com>
2018-08-03 17:46:50 +0100

    Initial commit with main projects

---
+++ MarvelWebApiTests/CharacterTests.cs
@@ -0,0 +1,26 @@
+﻿using MarvelWebApi.Entities;
+using NUnit.Framework;
+using Shouldly;
+using System;
+using System.Linq;
+
+namespace MarvelWebApiTests
+{
+    [TestFixture]
+    public class CharacterTests
+    {
+        [Test]
+        public void Captain_America()
+        {
+            // Arrange
+            var captainAmerica = new Character() { Name = "Captain America", Roles = new[] { Character.RoleEnum.Defense, Character.RoleEnum.Purge } };
+
+            // Act
+            // Assert
+            captainAmerica.Name.ShouldBe("Captain America");
+            captainAmerica.Roles.Count().ShouldBe(2);
+            captainAmerica.Roles.ShouldContain(r => r == Character.RoleEnum.Defense);
+            captainAmerica.Roles.ShouldContain(r => r == Character.RoleEnum.Purge);
+        }
+    }
+}
---
+++ MarvelWebApiTests/CharactersControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using MarvelWebApi.Controllers;
+using NUnit.Framework;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarvelWebApiTests
+{
+    [TestFixture]
+    public class CharactersControllerTests
+    {
+        [Test]
+        public void Characters_Get_no_parameters()
+        {
+            // Arrange
+            var controller = new CharactersController();
+
+            // Act
+            var result = controller.Get();
+
+            // Assert
+            result.Value.ShouldContain(s => s == "value1");
+            result.Value.ShouldContain(s => s == "value2");
+        }
+    }
+}
